634b532efcc586534b770838018f8bed0a861163
ezgikardes <ezgikardes@gmail.com>
2025-01-26 20:56:15 +0100

    add prop-types

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Star from "../components/Star";
+import PropTypes from "prop-types";
 
 const containerStyle = {
   display: "flex",
@@ -10,6 +11,16 @@ const containerStyle = {
 const starContainerStyle = {
   display: "flex",
   gap: "4px",
+};
+
+StarRating.propTypes = {
+  maxRating: PropTypes.number,
+  defaultRating: PropTypes.number,
+  color: PropTypes.string,
+  size: PropTypes.number,
+  messages: PropTypes.array,
+  className: PropTypes.string,
+  oneSetRating: PropTypes.func,
 };
 
 export default function StarRating({
