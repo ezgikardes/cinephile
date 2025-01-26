f05d9a339891254ce979a262c435f67974976e7a
ezgikardes <ezgikardes@gmail.com>
2025-01-26 20:33:34 +0100

    add more props for customizable scales

--- src/index.js
+++ src/index.js
@@ -3,13 +3,20 @@ import ReactDOM from "react-dom/client";
 //import "./styles/index.css";
 import App from "./App";
 import StarRating from "./components/StarRating";
+import Test from "./components/Test";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <StarRating maxRating={5} />
     <StarRating maxRating={10} />
-    <StarRating />
+    <StarRating
+      color="red"
+      className="test"
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      defaultRating={3}
+    />
+    <Test />
     {/* <App /> */}
   </React.StrictMode>
 );
