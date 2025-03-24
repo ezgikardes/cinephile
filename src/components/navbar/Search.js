7f208f3da45929416a9de15c0bde0c025c6a6af7
ezgikardes <ezgikardes@gmail.com>
2025-03-24 22:17:59 +0100

    feat: Add Enter key listener to focus input and clear query

--- src/components/navbar/Search.js
+++ src/components/navbar/Search.js
@@ -9,9 +9,22 @@ export default function Search({ query, setQuery }) {
   //   el.focus();
   // }, []);
 
-  useEffect(function () {
-    inputEl.current.focus();
-  }, []);
+  useEffect(
+    function () {
+      function callback(e) {
+        if (document.activeElement === inputEl.current) return;
+
+        if (e.code === "Enter") {
+          inputEl.current.focus();
+          setQuery("");
+        }
+      }
+
+      document.addEventListener("keydown", callback);
+      return () => document.addEventListener("keydown", callback);
+    },
+    [setQuery]
+  );
 
   return (
     <input
